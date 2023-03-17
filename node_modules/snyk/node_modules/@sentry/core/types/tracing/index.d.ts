676a26f86f140eed15b50dd46c140bb8b6e8369c
Punit Sharma <punit@tecorb.co>
2023-03-17 14:57:24 +0530

    change in headers data

--- node_modules/snyk/node_modules/@sentry/core/types/tracing/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { startIdleTransaction, addTracingExtensions } from './hubextensions';
-export { IdleTransaction, TRACING_DEFAULTS } from './idletransaction';
-export { Span, spanStatusfromHttpCode } from './span';
-export { Transaction } from './transaction';
-export { extractTraceparentData, getActiveTransaction, stripUrlQueryAndFragment, TRACEPARENT_REGEXP } from './utils';
-export { SpanStatus } from './spanstatus';
-export type { SpanStatusType } from './span';
-//# sourceMappingURL=index.d.ts.map
